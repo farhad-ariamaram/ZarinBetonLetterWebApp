a460c5e5d97766b37a7bf85cbe89f821f997199a
Farhad Ariamaram <farhad.ariamaram@gmail.com>
2022-09-05 11:23:00 +0430

    uploads remains

--- ZarinBetonLetterWebApp/Pages/EditReceivedMail.cshtml.cs
+++ ZarinBetonLetterWebApp/Pages/EditReceivedMail.cshtml.cs
@@ -4,13 +4,38 @@ using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
+using ZarinBetonLetterWebApp.Models;
 
 namespace ZarinBetonLetterWebApp.Pages
 {
     public class EditReceivedMailModel : PageModel
     {
-        public void OnGet()
+        AppDbContext _context;
+
+        public EditReceivedMailModel()
         {
+            _context = new AppDbContext();
+            _context.Database.EnsureCreated();
+        }
+
+        [BindProperty]
+        public ReceivedMail _receivedMail { get; set; }
+
+        public async Task<IActionResult> OnGet(int id)
+        {
+            _receivedMail = await _context.ReceivedMails.FindAsync(id);
+            if (_receivedMail == null)
+            {
+                return NotFound();
+            }
+
+            return Page();
+        }
+
+        public async Task OnPost()
+        {
+            _context.ReceivedMails.Update(_receivedMail);
+            await _context.SaveChangesAsync();
         }
     }
 }
